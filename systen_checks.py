b6a8071af523c78ea0382548e035ee668f8f8126
Shahar <theLittleCitizen12@gmail.com>
2022-04-18 23:15:12 +0300

    [ADD] run shell function to utils and implemnt her in system checks

--- systen_checks.py
+++ systen_checks.py
@@ -2,8 +2,7 @@ import windows_tools.antivirus
 import windows_tools.windows_firewall
 import windows_tools.powershell
 import platform
-from utils import write_to_file
-import subprocess
+from utils import write_to_file, run_shell_command
 
 
 def system_version():
@@ -16,29 +15,12 @@ def anti_virus():
 
 
 def dok():
-    output = subprocess.run([
-        'powershell.exe',
-        '-noprofile',
-        '-executionpolicy',
-        '-bypass',
-        '-c',
-        "Get-ItemProperty -PATH 'HKLM:\\SYSTEM\\CurrentControlSet\\Enum\\USBSTOR\\*\\*' | Select FriendlyName"
-    ],
-        capture_output=True)
+    output = run_shell_command("Get-ItemProperty -PATH 'HKLM:\\SYSTEM\\CurrentControlSet\\Enum\\USBSTOR\\*\\*' | Select FriendlyName")
     write_to_file("DOK", f"{output.stdout.decode('oem')}")
 
 def chrome_version():
-    output = subprocess.run([
-        'powershell.exe',
-        '-noprofile',
-        '-executionpolicy',
-        '-bypass',
-        '-c',
-        "reg query 'HKLM\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Google Chrome'"
-    ],
-        capture_output=True)
+    output= run_shell_command("reg query 'HKLM\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Google Chrome'")
     write_to_file("CHROME VERSION", f"{output.stdout.decode('oem')}")
-
 
 
 def windows_firewall_is_on():
@@ -47,53 +29,22 @@ def windows_firewall_is_on():
 
 
 def windows_firewall_policy():
-    output = subprocess.run([
-        'powershell.exe',
-        '-noprofile',
-        '-executionpolicy',
-        '-bypass',
-        '-c',
-        'Get-NetFirewallRule'
-    ],
-        capture_output=True)
+    output = run_shell_command("Get-NetFirewallRule")
     write_to_file("WINDOWS FIREWALL POLICY", f"{output.stdout.decode('oem')}")
 
 
 def password_policy():
-    output = subprocess.run([
-        'powershell.exe',
-        '-noprofile',
-        '-executionpolicy',
-        '-bypass',
-        '-c',
-        'net accounts'
-    ],
-        capture_output=True)
+    output = run_shell_command("net accounts")
     write_to_file("PASSWORD POLICY", f"{output.stdout.decode('oem')}")
 
+
 def login_events():
-    output = subprocess.run([
-        'powershell.exe',
-        '-noprofile',
-        '-executionpolicy',
-        '-bypass',
-        '-c',
-        'get-eventlog security  | where-object {$_.EventID -eq  "4625"} | select TimeGenerated |fl'
-    ],
-        capture_output=True)
+    output = run_shell_command("get-eventlog security  | where-object {$_.EventID -eq  '4625'} | select TimeGenerated |fl")
     write_to_file("FAILED LOGIN EVENTS", f"{output.stdout.decode('oem')}")
 
 
 def reboot_events():
-    output = subprocess.run([
-        'powershell.exe',
-        '-noprofile',
-        '-executionpolicy',
-        '-bypass',
-        '-c',
-        'get-eventlog system | where-object {$_.eventid -eq 1074} | select Timegenerated, EntryType, Message'
-    ],
-        capture_output=True)
+    output = run_shell_command("get-eventlog system | where-object {$_.eventid -eq 1074} | select Timegenerated, EntryType, Message")
     write_to_file("REBOOT EVENTS",f"{output.stdout.decode('oem')}")
 
 
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import subprocess
 from datetime import date
 
 
@@ -6,3 +7,15 @@ def write_to_file(check_name, check_result):
     file1 = open(f"C:\\Users\\Shahar\\Desktop\\{today}.txt", "a")
     file1.write(f"{check_name}: {check_result}\n")
     file1.close()
+
+
+def run_shell_command(command):
+    output = subprocess.run([
+        'powershell.exe',
+        '-noprofile',
+        '-executionpolicy',
+        '-bypass',
+        '-c',
+        command],
+        capture_output=True)
+    return output
