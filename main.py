1e64fdbdfab709dd9b3f44fa622157072432df58
Shahar <theLittleCitizen12@gmail.com>
2022-05-03 20:52:34 +0300

    [ADD] readme

--- main.py
+++ main.py
@@ -2,29 +2,7 @@ import sys
 import win32serviceutil  # ServiceFramework and commandline helper
 import win32service  # Events
 import servicemanager  # Simple setup and logging
-import socket
 from systen_checks import password_policy,dok, chrome_version,system_version,anti_virus,windows_firewall_policy,windows_firewall_is_on, login_events, reboot_events
-from utils import write_to_file
-"""
-to install the service:
-1. run from terminal/cmd the command : pip install -r requirements.txt
-2. cd to folder where you save the current project
-3.run from cmd/terminal: 
-pyinstaller.exe --onefile --runtime-tmpdir=. --hidden-import win32timezone main.py
-dist\main.exe install
-dist\main.exe start
-6. to insure the service started run: mmc Services.msc
-
-debug the service:
-run from cmd: dist\main.exe debug
-
-stop the service:
-run from cmd: dist\main.exe stop
-
-uninstall:
-dist\main.exe remove
-
-"""
 
 class MyService:
     """Silly little application stub"""
@@ -36,7 +14,6 @@ class MyService:
         """Main service loop. This is where work is done!"""
         self.running = True
         while self.running:
-            write_to_file("computer name: ", f"{socket.gethostname()}")
             system_version()
             anti_virus()
             windows_firewall_is_on()
